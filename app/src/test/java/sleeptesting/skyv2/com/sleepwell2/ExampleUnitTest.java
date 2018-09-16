c4ea56ed54ae505bca5040d1cbf5395fc9f1b070
febwinter <skyv2080@gmail.com>
2018-09-17 01:04:22 +0900

    Initial commit

---
+++ app/src/test/java/sleeptesting/skyv2/com/sleepwell2/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package sleeptesting.skyv2.com.sleepwell2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
